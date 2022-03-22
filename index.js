845f713260baa17957490d88e3061540b030b2f2
md-abdul-ahammed <shagarmd2082@gmail.com>
2022-03-23 01:38:40 +0600

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
-const PORT = process.env || 4000;
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
